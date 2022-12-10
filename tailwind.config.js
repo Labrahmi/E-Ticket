13a3afe2f24828099c59880ba5c71eee1b029ec3
youssef labrahmi <ylabrahm@c2r6p8.1337.ma>
2022-12-10 23:38:32 +0100

    main

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
